7f7631fd28b28e820a342c69f3fb4d56eae48ba7
csauses <coreymcnamara123@gmail.com>
2024-10-11 10:35:12 -0400

    assignemnt 2-3

---
+++ # Furniture.py
@@ -0,0 +1,17 @@
+# Furniture.py - This program calculates profits and sales prices for a furniture company.
+
+itemName = "TV Stand"
+retailPrice = 325.00
+wholesalePrice = 200.00
+
+profit = retailPrice - wholesalePrice
+salePrice = retailPrice * 0.75
+saleProfit = salePrice - wholesalePrice
+
+
+print("Item Name: " + itemName)
+print("Retail Price: $" + str(retailPrice))
+print("Wholesale Price: $" + str(wholesalePrice))
+print("Profit: $" + str(profit))
+print("Sale Price: $" + str(salePrice))
+print("Sale Profit: $" + str(saleProfit))
